151d1768af1e2fb1e2347f38a7d3ce3d6541cd47
Shellking <b.szabiszabi@gmail.com>
2025-03-29 17:45:15 +0100

    two method added

--- src/main/java/org/study/studyblogapi/service/IUserService.java
+++ src/main/java/org/study/studyblogapi/service/IUserService.java
@@ -1,9 +1,13 @@
 package org.study.studyblogapi.service;
 
 import org.study.studyblogapi.model.dto.ChangePasswordRequest;
+import org.study.studyblogapi.model.dto.UserResponse;
+import org.study.studyblogapi.model.entity.User;
 
 import java.security.Principal;
 
 public interface IUserService {
     void changePassword(ChangePasswordRequest request, Principal connectedUser);
+    UserResponse getAuthenticatedUserResponse();
+    User getAuthenticatedUser();
 }
