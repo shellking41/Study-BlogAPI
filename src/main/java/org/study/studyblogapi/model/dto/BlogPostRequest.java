5bc3f6c27a737432459622dc1a372d46c4bf3dc5
Shellking <b.szabiszabi@gmail.com>
2025-03-29 13:00:44 +0100

    BlogPostDtos Added

---
+++ src/main/java/org/study/studyblogapi/model/dto/BlogPostRequest.java
@@ -0,0 +1,17 @@
+package org.study.studyblogapi.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BlogPostRequest {
+    private String title;
+    private String content;
+    private String category;
+
+}
---
+++ src/main/java/org/study/studyblogapi/model/dto/BlogPostResponse.java
@@ -0,0 +1,21 @@
+package org.study.studyblogapi.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BlogPostResponse {
+    private String title;
+    private String content;
+    private String category;
+    private Integer createdBy;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
