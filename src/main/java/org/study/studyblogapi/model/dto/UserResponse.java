a210134cf4ce3c390440c9c4a8598f23a01dc78f
Shellking <b.szabiszabi@gmail.com>
2025-03-29 17:47:46 +0100

    UserResponse dto added

---
+++ src/main/java/org/study/studyblogapi/model/dto/UserResponse.java
@@ -0,0 +1,22 @@
+package org.study.studyblogapi.model.dto;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.study.studyblogapi.model.enums.Role;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    Integer id;
+    String firstname;
+    String lastname;
+    String email;
+    @Enumerated(EnumType.STRING)
+    Role role;
+}
