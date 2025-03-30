dd9cb5888f24b127bdfc50e21dd4be455c601de5
Shellking <b.szabiszabi@gmail.com>
2025-03-30 17:34:21 +0200

    now the frontend get the post id

--- src/main/java/org/study/studyblogapi/mapper/BlogPostMapper.java
+++ src/main/java/org/study/studyblogapi/mapper/BlogPostMapper.java
@@ -1,15 +1,17 @@
 package org.study.studyblogapi.mapper;
 
-import org.study.studyblogapi.model.dto.BlogPostRequest;
-import org.study.studyblogapi.model.dto.BlogPostResponse;
+import org.study.studyblogapi.model.dto.request.BlogPostRequest;
+import org.study.studyblogapi.model.dto.response.BlogPostResponse;
 import org.study.studyblogapi.model.entity.BlogPost;
+import org.study.studyblogapi.model.entity.User;
 
 public class BlogPostMapper {
 
-    public static BlogPost toEntity(BlogPostRequest request) {
+    public static BlogPost toEntity(BlogPostRequest request, User user) {
         return BlogPost.builder()
                 .category(request.getCategory())
                 .title(request.getTitle())
+                .author(user)
                 .content(request.getContent())
                 .build();
 
@@ -18,6 +20,7 @@ public class BlogPostMapper {
 
     public static BlogPostResponse toResponse(BlogPost blogPost) {
         return BlogPostResponse.builder()
+                .id(blogPost.getId())
                 .category(blogPost.getCategory())
                 .title(blogPost.getTitle())
                 .content(blogPost.getContent())
