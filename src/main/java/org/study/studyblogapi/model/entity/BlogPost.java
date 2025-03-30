1de6109a91ff04b95da02f53d8feb74b1c685ea8
Shellking <b.szabiszabi@gmail.com>
2025-03-30 17:32:35 +0200

    cascade added to the many to many relations

--- src/main/java/org/study/studyblogapi/model/entity/BlogPost.java
+++ src/main/java/org/study/studyblogapi/model/entity/BlogPost.java
@@ -26,11 +26,11 @@ public class BlogPost  extends  BaseEntity{
     @OneToMany(mappedBy = "blogPost",cascade = CascadeType.ALL,orphanRemoval = true)
     private List<Comment> comments;
 
-    @ManyToMany
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     @JoinTable(
             name = "blog_posts_tags",
-            joinColumns = @JoinColumn(name="blog_post_id",nullable = false),
-            inverseJoinColumns = @JoinColumn(name="tag_id",nullable = false)
+            joinColumns = @JoinColumn(name= "blog_post_id",nullable = false),
+            inverseJoinColumns = @JoinColumn(name= "tag_id",nullable = false)
     )
     private List<Tag> tags;
 
@@ -38,11 +38,11 @@ public class BlogPost  extends  BaseEntity{
     @JoinColumn(name = "user_id",nullable = false)
     private User author;
 
-    @ManyToMany
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     @JoinTable(
-            name="likes",
+            name= "likes",
             joinColumns = @JoinColumn(name = "likedPost_id",nullable = false),
-            inverseJoinColumns = @JoinColumn(name="likedByUser_id",nullable = false)
+            inverseJoinColumns = @JoinColumn(name= "likedByUser_id",nullable = false)
     )
     private List<User> likedByUsers;
 
