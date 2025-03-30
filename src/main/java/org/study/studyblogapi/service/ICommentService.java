c7ea4db50cd6a02efb40b2fee8453326a3fbe459
Shellking <b.szabiszabi@gmail.com>
2025-03-30 17:38:30 +0200

    new AddComment endpoint added

--- src/main/java/org/study/studyblogapi/service/ICommentService.java
+++ src/main/java/org/study/studyblogapi/service/ICommentService.java
@@ -1,4 +1,11 @@
 package org.study.studyblogapi.service;
 
+import org.study.studyblogapi.model.dto.request.CommentRequest;
+import org.study.studyblogapi.model.dto.response.CommentResponse;
+
 public interface ICommentService {
+
+     CommentResponse addComment(Long postId, CommentRequest request);
+
+
 }
