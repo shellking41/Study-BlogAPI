d1bf98059f346938bc5a6195bac379dd349f682c
Shellking <b.szabiszabi@gmail.com>
2025-04-04 19:14:13 +0200

    Button.test renamed to navbutton

--- src/main/resources/static/Study-BlogFrontend/src/components/tests/NavButton.test.jsx
+++ src/main/resources/static/Study-BlogFrontend/src/components/tests/NavButton.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Button from '../Button';
+import Button from '../NavButton.jsx';
 import userEvent from '@testing-library/user-event';
 import { vi } from 'vitest';
 
