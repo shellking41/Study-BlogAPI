e2f6fe361db36f0021a01110631f32b4a7218709
Shellking <b.szabiszabi@gmail.com>
2025-04-04 19:14:42 +0200

    custom form added

---
+++ src/main/resources/static/Study-BlogFrontend/src/services/Form.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Header from '../components/Header.jsx';
+import style from './styles/FormStyle.module.css';
+
+function Form({ inputs, headers, onSubmit, className }) {
+
+  const [formData, setFormData] = useState(
+    inputs.reduce((acc, input) => ({ ...acc, [input.name]: '' }), {}),
+  );
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    console.log(formData);
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <form onSubmit={(e) => onSubmit(e, formData)} className={className}>
+      <div className={style.headers}>
+        {headers && headers.length > 0 && headers.map((header) => {
+          return (
+            <div key={header.id} className={header.className}>
+              <Header tag={header.tag} text={header.text} />
+            </div>
+          );
+        })}
+      </div>
+      <div className={style.inputs}>
+        {inputs && inputs.length > 0 && inputs.map((input) => {
+          return (
+            <input key={input.id} placeholder={input.placeholder} type={input.type} className={input.className}
+                   name={input.name}
+                   onChange={handleChange} />
+          );
+        })}
+      </div>
+    </form>
+  );
+}
+
+export default Form;
