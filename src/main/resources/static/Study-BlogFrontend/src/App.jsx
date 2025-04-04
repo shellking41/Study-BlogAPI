d5e4bd91a81f5d267a91bfb65f5ea222ba67ac6d
Shellking <b.szabiszabi@gmail.com>
2025-04-04 19:15:15 +0200

    routing added

--- src/main/resources/static/Study-BlogFrontend/src/App.jsx
+++ src/main/resources/static/Study-BlogFrontend/src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Button from "./components/Button.jsx";
-import Input from "./components/Input.jsx";
+import {BrowserRouter, Routes} from "react-router-dom";
+import MainLayoutRoute from "./routes/MainLayoutRoute.jsx";
+
 
 function App() {
 
 
   return (
     <>
-      <Button  type={"button"} disabled={"fasz"}><h1>Click me</h1></Button>
-      <Input placeholder={"xd"} value={"jo"}/>
+      <BrowserRouter>
+          <Routes>
+              {MainLayoutRoute}
+          </Routes>
+      </BrowserRouter>
     </>
   )
 }
